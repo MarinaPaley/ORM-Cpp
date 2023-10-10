87a23f21952bffaf55e86fb5d5b3374d67657a1d
MarinaPaley <marina_paley@mail.ru>
2023-10-10 18:49:51 +0300

    Add project files.

---
+++ DomainTest/DomainTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DomainTest
+{
+	TEST_CLASS(DomainTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
